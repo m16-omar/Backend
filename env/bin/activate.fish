7c85404716dceea383173a56c1e02753aa7686fb
Umar Abdullahi <68796054+m16-omar@users.noreply.github.com>
2024-10-31 21:45:23 +0100

    Add files via upload

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -33,11 +33,11 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/kingsmactec/Downloads/development/apps /emart_store_backend/env"
+set -gx VIRTUAL_ENV "/Users/kingsmactec/Downloads/development/backends/emart_store_backend/myenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
-set -gx VIRTUAL_ENV_PROMPT "env"
+set -gx VIRTUAL_ENV_PROMPT "myenv"
 
 # Unset PYTHONHOME if set.
 if set -q PYTHONHOME
@@ -57,7 +57,7 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Output the venv prompt; color taken from the blue of the Python logo.
-        printf "%s(%s)%s " (set_color 4B8BBE) "env" (set_color normal)
+        printf "%s(%s)%s " (set_color 4B8BBE) "myenv" (set_color normal)
 
         # Restore the return status of the previous command.
         echo "exit $old_status" | .
